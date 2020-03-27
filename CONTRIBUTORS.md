422cdd1f094ee8bcaa4ac673a887a9ce531bd981
Paul Hartwell <phartwel@gmail.com>
2020-03-27 15:06:40 -0500

    BroadcastableValueSets threading issue (#84)
    * Spark encoders not guaranteed to be thread safe.
    
    * Added to CONTRIBUTORS.md
    
    * Added comment to point back to issue and added changed log4j.properties to WARN level.
    
    Co-authored-by: Paul Hartwell <paul.hartwell@cerner.com>

--- CONTRIBUTORS.md
+++ CONTRIBUTORS.md
@@ -4,6 +4,8 @@ Cerner Corporation
 
 - Ryan Brush [@rbrush]
 - Aleksander Eskilson [@bdrillard]
+- Paul Hartwell [@paulhartwell]
 
 [@rbrush]: https://github.com/rbrush
 [@bdrillard]: https://github.com/bdrillard
+[@paulhartwell]: https://github.com/PaulHartwell
--- bunsen-spark/src/test/java/com/cerner/bunsen/spark/codes/broadcast/BroadcastableValueSetsTest.java
+++ bunsen-spark/src/test/java/com/cerner/bunsen/spark/codes/broadcast/BroadcastableValueSetsTest.java
@@ -9,8 +9,15 @@ import com.cerner.bunsen.spark.codes.systems.Snomed;
 import com.google.common.collect.ImmutableSet;
 import java.io.IOException;
 import java.nio.file.Files;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Map;
 import java.util.Set;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.concurrent.Future;
+import java.util.concurrent.TimeUnit;
+
 import org.apache.spark.sql.SparkSession;
 import org.junit.AfterClass;
 import org.junit.Assert;
@@ -223,5 +230,87 @@ public class BroadcastableValueSetsTest {
     Assert.assertTrue(priorityValues.containsKey("http://hl7.org/fhir/v3/ActPriority"));
     Assert.assertTrue(ImmutableSet.of("EM")
         .containsAll(priorityValues.get("http://hl7.org/fhir/v3/ActPriority")));
+  }
+
+  /**
+   * Added to address https://github.com/cerner/bunsen/issues/85
+   */
+  @Test
+  public void testThreadSafety() {
+    List<Future> futures = new ArrayList<>();
+    final ExecutorService pool = Executors.newFixedThreadPool(5);
+
+    for (int i = 0; i < 5; i++) {
+      Future future = pool.submit(new BroadcastValueSetsThread(spark, i));
+      futures.add(future);
+    }
+
+    pool.shutdown();
+
+    boolean finished = false;
+
+    try {
+      finished = pool.awaitTermination(1, TimeUnit.MINUTES);
+    } catch (InterruptedException exception) {
+      throw new RuntimeException(exception);
+    }
+
+    if (!finished) {
+      throw new RuntimeException("Timeout occurred while awaiting completion.");
+    }
+
+    futures.stream()
+        .forEach(future -> {
+          try {
+            future.get();
+          } catch (Exception exception) {
+            throw new RuntimeException(exception);
+          }
+        });
+  }
+
+  protected class BroadcastValueSetsThread extends Thread {
+    private final SparkSession session;
+    private final int threadNum;
+
+    protected BroadcastValueSetsThread(final SparkSession session, final int threadNum) {
+      this.session = session.cloneSession();
+      this.threadNum = threadNum;
+    }
+
+    @Override
+    public void run() {
+      String bp = "bp" + threadNum;
+      String leukocytes = "leukocytes" + threadNum;
+      String priorities = "priorities" + threadNum;
+
+      BroadcastableValueSets valueSets = BroadcastableValueSets.newBuilder()
+          .addCode(bp,
+              Loinc.LOINC_CODE_SYSTEM_URI,
+              "8462-4")
+          .addDescendantsOf(leukocytes,
+              Loinc.LOINC_CODE_SYSTEM_URI,
+              "LP14419-3",
+              Loinc.LOINC_HIERARCHY_URI)
+          .addReference(priorities,
+              "http://hl7.org/fhir/ValueSet/v3-ActPriority")
+          .build(session, mockValueSets, Hierarchies.getDefault(session));
+
+      Assert.assertNotNull(valueSets);
+
+      Assert.assertTrue(ImmutableSet.of(bp, leukocytes, priorities, "types")
+          .containsAll(valueSets.getReferenceNames()));
+
+      Map<String,Set<String>> leukocyteValues = valueSets.getValues(leukocytes);
+      Map<String,Set<String>> priorityValues = valueSets.getValues(priorities);
+
+      Assert.assertTrue(leukocyteValues.containsKey("http://loinc.org"));
+      Assert.assertTrue(ImmutableSet.of("LP14419-3", "5821-4")
+          .containsAll(leukocyteValues.get("http://loinc.org")));
+
+      Assert.assertTrue(priorityValues.containsKey("http://hl7.org/fhir/v3/ActPriority"));
+      Assert.assertTrue(ImmutableSet.of("EM")
+          .containsAll(priorityValues.get("http://hl7.org/fhir/v3/ActPriority")));
+    }
   }
 }
